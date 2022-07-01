254f501ea19ba1df0336d8f1b65b70fa908e2831
LAPTOP-DEHRM2D8\wuquan <17324432872@163.com>
2022-07-01 17:46:26 +0800

    fea:   1.添加跳转路由手势

---
+++ lib/CustomWidget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TopBoxA extends StatefulWidget {
+  TopBoxA({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _TopBoxAState();
+}
+
+class _TopBoxAState extends State<TopBoxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
